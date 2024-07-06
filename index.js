936a42b912c7b2bdca37682bf154e690785c228a
Imadabattuni Naveen Kumar <imadabattunink@gmail.com>
2024-07-06 20:14:56 +0530

    explore react functional component, react element & component composition

--- index.js
+++ index.js
@@ -22,6 +22,34 @@ const Heading= () =>(
 
 const Title= () => (<h1 id="title">Title</h1>)
 const root1= ReactDOM.createRoot(document.getElementById("root1"));
-root1.render(<Heading/>);
 
-const x=5;
+
+const reactElement1=(
+    <div id="re1">
+        <h1>this is react element 1</h1>
+        <h1>this is part of react element 1</h1>
+    </div>
+);
+
+const ReactFunctionalComponent1= () => (
+    <div id="rfc1">
+        {reactElement1}
+        <h1>thi is part of react component 1</h1>
+    </div>
+);
+
+const reactElement2=(
+    <div id="re1">
+        <h1>this is react element 2</h1>
+        <ReactFunctionalComponent1/>
+        <h1>this is part of react element 2</h1>
+    </div>
+);
+
+const ReactFunctionalComponent2= () => (
+    <div id="rfc1">
+        {reactElement1}
+        <h1>thi is part of react component 2</h1>
+    </div>
+);
+root1.render(reactElement2);
